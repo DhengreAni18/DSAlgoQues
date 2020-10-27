64b4b9c6b40b84b4ed1b79b9e88dd0f56583f128
AnimeshDhenrge <dhengreani18@gmail.com>
2020-10-28 00:53:51 +0530

    Added

--- FINAL450/3_kthsmallest.java
+++ FINAL450/3_kthsmallest.java
@@ -1,10 +1,10 @@
 import java.util.*;
-class boilerplate {
+class kthsmallest {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
 
-        while(t-- > 0) {
+        while(t-- >= 0) {
             int N = s.nextInt();
             int[] arr = new int[N];
             for (int i = 0; i < N; i++) {
@@ -14,6 +14,11 @@ class boilerplate {
 
             helper(arr,k);
         }
+        
         s.close();
+    }
+
+    public static void helper(int[] arr , int k) {
+        
     }
 }
