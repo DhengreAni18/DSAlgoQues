bf72f0dbe000ec4d0d5bfa511e4e230ebcb64ee2
AnimeshDhenrge <dhengreani18@gmail.com>
2020-10-27 20:18:51 +0530

    Added Boilerplate code

---
+++ boilerplate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class boilerplate {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+
+        while(t-- > 0) {
+            int N = s.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = s.nextInt();
+            }
+            int k = s.nextInt();
+
+            helper(arr,k);
+        }
+    }
+}
