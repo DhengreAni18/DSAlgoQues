285b4ec36961583028b993118043fa56c240bad3
Animesh Dhengre <dhengreani18@gmail.com>
2020-11-01 15:44:45 +0530

    Changes to boilerplate

--- boilerplate.java
+++ boilerplate.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 class boilerplate {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
 
-        while(t-- > 0) {
+        while (t-- > 0) {
             int N = s.nextInt();
             int[] arr = new int[N];
             for (int i = 0; i < N; i++) {
@@ -12,8 +13,12 @@ class boilerplate {
             }
             int k = s.nextInt();
 
-            helper(arr,k);
+            helper(arr, k);
         }
         s.close();
+    }
+
+    public static void helper(int[] arr, int k) {
+
     }
 }
